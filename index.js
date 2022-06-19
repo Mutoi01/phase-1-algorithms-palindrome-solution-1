871513960843e1c380fbd40902935f293596c2eb
Mutoi01 <pngando9@gmail.com>
2022-06-19 10:15:27 +0300

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+function reverseWord(word){
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const rev = reverseWord(word);
+
+  return word ===  rev;
+
 }
+
 
 /* 
   Add your pseudocode here
+  should write an algorithim that checks if a word is a palindrome. 
+  This word should retain the same meaning when reversed like the word eye
 */
 
 /*
@@ -20,6 +31,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("eye"));
 }
 
 module.exports = isPalindrome;
